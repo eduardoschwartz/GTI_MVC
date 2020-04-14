350e959def1196760f46bcaef25063e365c499c1
eduardoschwartz <39421285+eduardoschwartz@users.noreply.github.com>
2020-04-14 09:55:31 -0300

    imovel.rpt

--- GTI_Models/ReportModels/Imovel_Detalhe.cs
+++ GTI_Models/ReportModels/Imovel_Detalhe.cs
@@ -1,13 +1,39 @@
 ﻿namespace GTI_Models.ReportModels {
     public class Imovel_Detalhe {
         public int Codigo { get; set; }
-        public string Proprietario { get; set; }
-        public string Cpf_Cnpj { get; set; }
         public string Inscricao { get; set; }
+        public string Situacao_Imovel { get; set; }
+        public string MT { get; set; }
+        public string Proprietario { get; set; }
+        public string Proprietario2 { get; set; }
         public string Endereco { get; set; }
+        public int Numero { get; set; }
+        public string Complemento { get; set; }
         public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Uf { get; set; }
         public string Cep { get; set; }
+        public string Quadra_Original { get; set; }
+        public string Lote_Original { get; set; }
+        public decimal Area_Terreno { get; set; }
+        public decimal Fracao_Ideal { get; set; }
+        public decimal Testada { get; set; }
+        public decimal Agrupamento { get; set; }
+        public decimal Soma_Fatores { get; set; }
+        public decimal Area_Predial { get; set; }
+        public string Imunidade { get; set; }
+        public string Reside_Imovel { get; set; }
+        public int Qtde_Edif { get; set; }
+        public string Isento_Cip { get; set; }
+        public string Topografia { get; set; }
+        public string Pedologia { get; set; }
+        public string Situacao { get; set; }
+        public string Uso_Terreno { get; set; }
+        public string Benfeitoria { get; set; }
+        public string Categoria { get; set; }
+        public string Condominio { get; set; }
+        public decimal Vvt { get; set; }
+        public decimal Vvp { get; set; }
+        public decimal Vvi { get; set; }
+        public decimal Iptu { get; set; }
+        public string Controle { get; set; }
     }
 }
