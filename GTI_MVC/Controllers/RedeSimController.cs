1dbb7819ad596a4c01df1dc45aa4b92a65f6b050
eduardoschwartz <39421285+eduardoschwartz@users.noreply.github.com>
2021-02-19 17:03:29 -0300

    redesim inicio

---
+++ GTI_MVC/Controllers/RedeSimController.cs
@@ -0,0 +1,62 @@
+﻿using GTI_Mvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+
+namespace GTI_MVC.Controllers {
+    public class RedeSimController : Controller
+    {
+
+        [Route("UploadFiles")]
+        [HttpGet]
+        public ActionResult UploadFiles() {
+            List<RedesimImportFilesViewModel> Lista_Files = new List<RedesimImportFilesViewModel>();
+            RedesimImportViewModel model = new RedesimImportViewModel() {
+                ListaArquivo = Lista_Files
+            };
+            return View(model);
+        }
+
+        [Route("UploadFiles")]
+        [HttpPost]
+        public ActionResult UploadFiles(RedesimImportViewModel model) {
+            List<RedesimImportFilesViewModel> Lista_Files = new List<RedesimImportFilesViewModel>();
+            int _id = 1;
+            string _msg = "",_tipo="";
+            DateTime _dataDe = DateTime.Now, _dataAte = DateTime.Now;
+            bool _ok=false;
+            foreach (var file in model.Files) {
+                var fileName= Path.GetFileName(file.FileName);
+                if (file.ContentLength > 0) {
+                    if (file.ContentType == "application/vnd.ms-excel") {
+                        _msg = "Arquivo importado";
+                        _ok = true;
+                        _tipo = "Licenciamento";
+                        
+                        var filePath = Path.Combine(Server.MapPath("~/Images"), fileName);
+//                        file.SaveAs(filePath);
+                    } else {
+                        _msg = "Arquivo inválido";
+                    }
+                } else {
+                    _msg = "Tamanho inválido";
+                }
+                RedesimImportFilesViewModel _reg = new RedesimImportFilesViewModel() {
+                    Id = _id,
+                    Nome=fileName,
+                    Mensagem=_msg,
+                    Valido=_ok,
+                    Tipo=_tipo,
+                    PeriodoDe=_dataDe,
+                    PeriodoAte=_dataAte
+                };
+                Lista_Files.Add(_reg);
+                _id++;
+            }
+            model.ListaArquivo = Lista_Files;
+            return View(model);
+      
+        }
+    }
+}
---
+++ GTI_MVC/ViewModels/RedesimImportViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace GTI_Mvc.ViewModels {
+    public class RedesimImportViewModel {
+        public IEnumerable<HttpPostedFileBase> Files { get; set; }
+        public List<RedesimImportFilesViewModel> ListaArquivo { get; set; }
+    }
+
+
+    public class RedesimImportFilesViewModel {
+        public int Id { get; set; }
+        public DateTime PeriodoDe { get; set; }
+        public DateTime PeriodoAte { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public bool Valido { get; set; }
+        public string Mensagem { get; set; }
+    }
+
+}
