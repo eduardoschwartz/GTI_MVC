d8b39681d485f3d35422b4066a1d4d3efcee5830
Eduardo Schwartz <eduardo.schwartz@gmail.com>
2021-06-25 10:27:01 -0300

    mobreq_sola

--- GTI_MVC/Controllers/MobReqController.cs
+++ GTI_MVC/Controllers/MobReqController.cs
@@ -50,27 +50,61 @@ namespace GTI_Mvc.Controllers
 
             TempData["cpfcnpj"] = model.CpfValue;
             TempData["evento"] = model.Evento_Codigo;
-            return RedirectToAction("Mobreq_solb");
+            if(model.Evento_Codigo==3)
+                return RedirectToAction("Mobreq_sola");
+            else {
+                return View(model);
+            }
         }
 
-        [Route("Mobreq_solb")]
+        [Route("Mobreq_sola")]
         [HttpGet]
-        public ActionResult Mobreq_solb() {
+        public ActionResult Mobreq_sola() {
             if(Session["hashid"] == null)
                 return RedirectToAction("Login","Home");
 
-            string _cpfcnpj = TempData["cpfcnpj"].ToString();
+            string _cpfcnpj = Functions.RetornaNumero( TempData["cpfcnpj"].ToString());
             int _evento = Convert.ToInt32(TempData["evento"]);
+            bool _bCpf = _cpfcnpj.Length == 11 ? true : false;
+
+            Empresa_bll empresaRepository = new Empresa_bll(_connection);
+            int _codigo = 0;
+            if(_bCpf)
+                _codigo = empresaRepository.ExisteEmpresaCpf(_cpfcnpj);
+            else
+                _codigo = empresaRepository.ExisteEmpresaCnpj(_cpfcnpj);
+
+            EmpresaStruct _dados = empresaRepository.Retorna_Empresa(_codigo);
+
+            string _rgie = "N/D";
+            if(_bCpf)
+                _rgie = string.IsNullOrEmpty( _dados.Rg) ? _rgie:_dados.Rg;
+            else
+                _rgie = string.IsNullOrEmpty(_dados.Inscricao_estadual) ? _rgie : _dados.Inscricao_estadual;
 
             MobReqViewModel model = new MobReqViewModel();
             Mobreq_bll mobreqRepository = new Mobreq_bll(_connection);
             List<Mobreq_evento> Lista = mobreqRepository.Lista_vento();
             ViewBag.ListaEvento = new SelectList(Lista,"Codigo","Descricao",1);
 
+
+            model.Razao_Social = _dados.Razao_social;
+            model.Codigo = _codigo;
+            model.Rg_IE = _rgie;
+            model.Atividade = _dados.Atividade_extenso;
             model.Evento_Codigo = _evento;
-            model.CpfValue = _cpfcnpj;
+            model.CpfValue = Functions.FormatarCpfCnpj( _cpfcnpj);
             return View(model);
         }
+
+        [Route("Mobreq_sola")]
+        [HttpPost]
+        public ActionResult Mobreq_sola(MobReqViewModel model) {
+
+
+            return View(model);
+        }
+
 
     }
 }
--- GTI_MVC/ViewModels/MobReqViewModel.cs
+++ GTI_MVC/ViewModels/MobReqViewModel.cs
@@ -6,5 +6,11 @@ namespace GTI_Mvc.ViewModels {
     public class MobReqViewModel {
         public int Evento_Codigo { get; set; }
         public string CpfValue { get; set; }
+        public string Razao_Social { get; set; }
+        public string Rg_IE { get; set; }
+        public string Atividade { get; set; }
+        public string Obs { get; set; }
+        public string Data_Evento { get; set; }
+        public int Codigo { get; set; }
     }
 }
