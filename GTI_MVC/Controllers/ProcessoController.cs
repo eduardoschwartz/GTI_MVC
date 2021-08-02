cd1c52edfc2c5994cb99a57214bd8c122c3982f1
Eduardo Schwartz <eduardo.schwartz@gmail.com>
2021-08-02 11:20:41 -0300

    novo processo inicio

---
+++ GTI_MVC/Controllers/ProcessoController.cs
@@ -0,0 +1,45 @@
+﻿using GTI_Bll.Classes;
+using GTI_Mvc.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GTI_MVC.Controllers
+{
+    public class ProcessoController : Controller    {
+        private readonly string _connection = "GTIconnectionTeste";
+
+        [Route("Processo_menu")]
+        [HttpGet]
+        public ActionResult Processo_menu() {
+            if (Session["hashid"] == null)
+                return RedirectToAction("Login", "Home");
+
+            return View();
+        }
+
+        [Route("Processo_tp")]
+        [HttpGet]
+        public ActionResult Processo_tp() {
+            if (Session["hashid"] == null)
+                return RedirectToAction("Login", "Home");
+            Processo2ViewModel model = new Processo2ViewModel();
+
+            return View(model);
+        }
+
+        [Route("Processo_tp")]
+        [HttpPost]
+        public ActionResult Processo_tp(Processo2ViewModel model) {
+            if (Session["hashid"] == null)
+                return RedirectToAction("Login", "Home");
+
+
+            return View(model);
+        }
+
+
+    }
+}
---
+++ GTI_MVC/ViewModels/Processo2ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GTI_Mvc.ViewModels {
+    public class Processo2ViewModel {
+        public string Tipo_Requerente { get; set; } 
+       
+    }
+}
