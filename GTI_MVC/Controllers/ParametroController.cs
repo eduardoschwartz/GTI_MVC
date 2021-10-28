71ca41aa6dc893c68af8225a69ad7878ba7378de
Eduardo Schwartz <eduardo.schwartz@gmail.com>
2021-10-28 16:49:50 -0300

    cadastro de bairros inicio

--- GTI_MVC/Controllers/ParametroController.cs
+++ GTI_MVC/Controllers/ParametroController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GTI_Bll.Classes;
+using GTI_Models.Models;
+using GTI_Mvc.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,26 @@ namespace GTI_Mvc.Controllers
     public class ParametroController : Controller    {
         private readonly string _connection = "GTIconnection";
 
+        [Route("Bairro_Edit")]
+        [HttpGet]
         public ActionResult Bairro_Edit()
         {
+            Endereco_bll enderecoRepository = new Endereco_bll(_connection);
+            List<Uf> listaUf = enderecoRepository.Lista_UF();
+            ViewBag.ListaUF = new SelectList(listaUf, "siglauf", "descuf");
             return View();
+        }
+
+        [Route("Bairro_Edit")]
+        [HttpPost]
+        public ActionResult Bairro_Edit(BairroViewModel model) {
+            return View(model);
+        }
+
+        public JsonResult Lista_Cidade(string uf) {
+            Endereco_bll enderecoRepository = new Endereco_bll(_connection);
+            List<Cidade> Lista = enderecoRepository.Lista_Cidade(uf);
+            return new JsonResult { Data = Lista, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
     }
 }
---
+++ GTI_MVC/ViewModels/BairroViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace GTI_Mvc.ViewModels {
+    public class BairroViewModel {
+        public string UF_Sigla { get; set; }
+        public string UF_Nome { get; set; }
+        public short Cidade_Codigo { get; set; }
+        public string Cidade_Nome { get; set; }
+        public short Bairro_Codigo { get; set; }
+        public string Bairro_Nome{ get; set; }
+    }
+}
